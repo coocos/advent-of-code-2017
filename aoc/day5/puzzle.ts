2e770595a9ac68c5e78a0cb4a4f2776ef8c66cdf
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2022-01-13 18:46:36 +0200

    Solve day 5 puzzles

---
+++ aoc/day5/puzzle.ts
@@ -0,0 +1,38 @@
+import { readFile } from "fs/promises";
+import { join } from "path";
+import assert from "assert";
+
+async function readInput() {
+  const input = await readFile(join(__dirname, "input.txt"), "utf-8");
+  return input.split("\n").map((line) => parseInt(line));
+}
+
+function jump(offsets: Array<number>, rule: (x: number) => number): number {
+  let index = 0;
+  let steps = 0;
+  while (index >= 0 && index < offsets.length) {
+    const next = index + offsets[index];
+    offsets[index] += rule(offsets[index]);
+    index = next;
+    steps += 1;
+  }
+  return steps;
+}
+
+async function solve() {
+  const offsets = await readInput();
+
+  // First part
+  assert.strictEqual(
+    jump([...offsets], () => 1),
+    325922
+  );
+
+  // Second part
+  assert.strictEqual(
+    jump([...offsets], (x) => (x >= 3 ? -1 : 1)),
+    24490906
+  );
+}
+
+solve();
