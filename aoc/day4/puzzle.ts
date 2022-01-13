5863e7441022288c7235e187d50828ad153d14c7
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2022-01-13 18:25:48 +0200

    Solve day 4 puzzles

---
+++ aoc/day4/puzzle.ts
@@ -0,0 +1,27 @@
+import { readFile } from "fs/promises";
+import { join } from "path";
+import assert from "assert";
+
+async function readInput() {
+  const input = await readFile(join(__dirname, "input.txt"), "utf-8");
+  const lines = input.split("\n");
+  return lines.map((line) => line.split(" "));
+}
+
+async function solve() {
+  const passphrases = await readInput();
+
+  // First part
+  const validForFirstPolicy = passphrases.filter(
+    (phrase) => new Set(phrase).size === phrase.length
+  );
+  assert.strictEqual(validForFirstPolicy.length, 455);
+
+  // Second part
+  const validForSecondPolicy = passphrases
+    .map((phrase) => phrase.map((word) => Array.from(word).sort().join("")))
+    .filter((phrase) => new Set(phrase).size === phrase.length);
+  assert.strictEqual(validForSecondPolicy.length, 186);
+}
+
+solve();
