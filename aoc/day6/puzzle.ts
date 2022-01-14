4261ee61a1fed16fbeba8eade4450eec97d46856
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2022-01-14 17:16:08 +0200

    Solve day 6 puzzles

---
+++ aoc/day6/puzzle.ts
@@ -0,0 +1,40 @@
+import { readFile } from "fs/promises";
+import { join } from "path";
+import assert from "assert";
+
+async function readInput() {
+  const input = await readFile(join(__dirname, "input.txt"), "utf-8");
+  return input.split(" ").map((value) => parseInt(value));
+}
+
+function distribute(banks: number[]) {
+  let bank = banks.findIndex((blocks) => blocks === Math.max(...banks));
+  let blocks = banks[bank];
+  banks[bank] = 0;
+  while (blocks > 0) {
+    bank++;
+    banks[bank % banks.length]++;
+    blocks--;
+  }
+}
+
+function loop(banks: number[]): number {
+  const seen = new Set<string>();
+  while (!seen.has(banks.join())) {
+    seen.add(banks.join());
+    distribute(banks);
+  }
+  return seen.size;
+}
+
+async function solve() {
+  const banks = await readInput();
+
+  // First part
+  assert.strictEqual(loop(banks), 12841);
+
+  // Second part
+  assert.strictEqual(loop(banks), 8038);
+}
+
+solve();
