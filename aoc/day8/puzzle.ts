fc1aca87994b4cbc87398083a28744ca19f52543
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2022-01-17 17:58:31 +0200

    Solve day 8 puzzles

---
+++ aoc/day8/puzzle.ts
@@ -0,0 +1,74 @@
+import { readFile } from "fs/promises";
+import { join } from "path";
+import assert from "assert";
+
+interface Instruction {
+  register: string;
+  command: "inc" | "dec";
+  value: number;
+  condition: {
+    register: string;
+    comparison: "==" | "!=" | ">" | "<" | ">=" | "<=";
+    value: number;
+  };
+}
+
+async function readInput(): Promise<Instruction[]> {
+  const input = await readFile(join(__dirname, "input.txt"), "utf-8");
+  return input.split("\n").map((line) => {
+    const match =
+      /([a-z]+) (inc|dec) (-?\d+) if ([a-z]+) ([<>=!]+) (-?\d+)/.exec(line);
+    if (match === null) {
+      throw new Error(`Failed to match ${line}`);
+    }
+    return {
+      register: match[1],
+      command: match[2] as Instruction["command"],
+      value: parseInt(match[3]),
+      condition: {
+        register: match[4],
+        comparison: match[5] as Instruction["condition"]["comparison"],
+        value: parseInt(match[6]),
+      },
+    };
+  });
+}
+
+async function solve() {
+  const instructions = await readInput();
+
+  const registers: { [name: string]: number } = instructions.reduce(
+    (registers, instruction) => {
+      return {
+        ...registers,
+        [instruction.register]: 0,
+        [instruction.condition.register]: 0,
+      };
+    },
+    {}
+  );
+
+  let highestValueEver = 0;
+
+  for (const instruction of instructions) {
+    const expression = `${registers[instruction.condition.register]} ${
+      instruction.condition.comparison
+    } ${instruction.condition.value}`;
+    if (eval(expression)) {
+      registers[instruction.register] +=
+        instruction.value * (instruction.command == "inc" ? 1 : -1);
+      highestValueEver = Math.max(
+        registers[instruction.register],
+        highestValueEver
+      );
+    }
+  }
+
+  // First part
+  assert.strictEqual(Math.max(...Object.values(registers)), 4832);
+
+  // Second part
+  assert.strictEqual(highestValueEver, 5443);
+}
+
+solve();
