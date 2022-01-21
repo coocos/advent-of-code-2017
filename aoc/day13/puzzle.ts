0f3162bbc3e951db6bee02ae1d14ef574061877b
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2022-01-21 17:32:23 +0200

    Solve day 13 puzzles

---
+++ aoc/day13/puzzle.ts
@@ -0,0 +1,33 @@
+import { readFile } from "fs/promises";
+import { join } from "path";
+import assert from "assert";
+
+async function readInput() {
+  const input = await readFile(join(__dirname, "input.txt"), "utf-8");
+  return input
+    .split("\n")
+    .map((line) => line.split(": ").map((value) => parseInt(value)));
+}
+
+async function solve() {
+  const scanners = await readInput();
+
+  // First part
+  const severity = scanners
+    .filter(([id, range]) => id % ((range - 1) * 2) === 0)
+    .reduce((total, [id, range]) => total + id * range, 0);
+  assert(severity === 1844);
+
+  // Second part
+  let delay = 0;
+  while (true) {
+    if (
+      scanners.every(([id, range]) => (delay + id) % ((range - 1) * 2) !== 0)
+    ) {
+      break;
+    }
+    delay++;
+  }
+  assert(delay === 3897604);
+}
+solve();
